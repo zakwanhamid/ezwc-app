47194b7d705722dcd8723ed2662adbb3ee0ec0fa
Zakwan Hamid <zakwanhamid@gmail.com>
2023-12-15 04:33:59 +0800

    first push

---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      headerShown: false,
+    });
+  } , [])
+
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
