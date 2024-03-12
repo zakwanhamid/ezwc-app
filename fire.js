a1a4e32aaf2f214508b75ad747d07709cd4b3fab
Zakwan Hamid <zakwanhamid@gmail.com>
2024-03-12 13:28:38 +0800

    succesfully save data to firestore but not image

--- fire.js
+++ fire.js
@@ -1,30 +1,42 @@
-import { FIREBASE_APP, FIREBASE_AUTH } from "./firebase";
+import { FIREBASE_APP, FIREBASE_AUTH, FIREBASE_DB } from "./firebase";
 import { getFirestore, collection, addDoc } from 'firebase/firestore'; // Import Firestore methods
+import { getStorage } from "firebase/storage";
 
 class Fire {
     constructor() {
         // Initialize Firebase app using the exported FIREBASE_APP
         this.firebaseApp = FIREBASE_APP;
         // Access Firestore database service using the exported FIREBASE_DB
-        this.firestore = getFirestore(FIREBASE_APP); // Initialize Firestore instance
+        this.firestore = FIREBASE_DB; // Initialize Firestore instance
     }
 
     addPost = async ({ text, images }) => {
-        // Get the current user from Firebase Authentication service
         const user = FIREBASE_AUTH.currentUser;
         const userId = user.uid;
-
+    
         const post = {
             text: text,
-            images: images,
             userId: userId,
-            timestamp: new Date() // Use JavaScript Date object
+            timestamp: new Date()
         };
-
+    
         try {
-            // Add the post to the Firestore collection
+            // Add the post details to Firestore
             const docRef = await addDoc(collection(this.firestore, 'posts'), post);
-            console.log("Document written with ID: ", docRef.id);
+            console.log("Document written with ID: ",docRef.id)
+            
+            // Upload images to Firebase Storage
+            // const imageUrls = [];
+            // for (const image of images) {
+            //     const imageRef = getStorage(FIREBASE_APP).ref().child(`images/${docRef.id}/${image.name}`);
+            //     await imageRef.put(image);
+            //     const imageUrl = await imageRef.getDownloadURL();
+            //     imageUrls.push(imageUrl);
+            // }
+    
+            // // Update the post with image URLs
+            // await docRef.update({ images: imageUrls });
+    
             return true;
         } catch (error) {
             console.error("Error adding post: ", error);
--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ import firebase from "firebase/app";
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,15 +24,4 @@ const firebaseConfig = {
 export const FIREBASE_APP  = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
-
-// Initialize Firebase
-// let app;
-// if(firebase.apps.length === 0) {
-//     app = firebase.initializeApp(firebaseConfig);
-// } else {
-//     app = firebase.app ()
-// }
-
-// const auth = firebase.auth()
-
-// export { auth };
+export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
