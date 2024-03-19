cdb86b035a3a87409c768714a03b8d3b4e49c800
Zakwan Hamid <zakwanhamid@gmail.com>
2024-03-20 03:23:45 +0800

    modulebackground settel

--- screens/ModuleScreens/ModuleBgScreen.js
+++ screens/ModuleScreens/ModuleBgScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { Entypo, FontAwesome, Ionicons } from '@expo/vector-icons';
@@ -26,6 +26,39 @@ const ModuleBgScreen = () => {
             <TouchableOpacity style={styles.postBtn}>
                 <FontAwesome name="map-o" size={20} color="black" />
             </TouchableOpacity>
+        </View>
+        <View>
+            <Image source={require('../../assets/topModuleImage.png')}
+            style={{height: 150, width: '100%'}}
+            />
+        </View>
+        <View style={{justifyContent: "center", alignItems:"center"}}>
+          <Text style={{fontSize: 22, fontWeight: 600, textAlign: "center", marginTop:20}}>
+            Zero Waste Campus Digital Module
+          </Text>
+          <Text style={{fontSize: 20, fontWeight: 400, textAlign: "center", marginBottom:20}}>
+            Module Background
+          </Text>
+          <Text style={styles.textBg}>
+          A series of data collections involving focus group discussion, 
+          in-depth interviews, and a survey with 393 respondents from USM, 
+          UKM, and UPM comprised of staff and students had been carried out from 2020-2022.
+          </Text>
+          <Text style={styles.textBg}>
+          The ten relevant factors shaping the university campus community's
+          sustainable pro-environmental behaviour towards Zero-Waste Campus 
+          have been identified, and a Zero-Waste Campus Framework has been proposed.
+          </Text>
+          <Text style={styles.textBg}>
+          This module was developed to publicise the research 
+          finding to increase public awareness and participation 
+          in promoting environmentally friendly behaviour toward a zero-waste goal.
+          </Text>
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={{fontSize: 16, fontWeight:600}} > GOT IT ! </Text>
+          </TouchableOpacity>
         </View>
     </SafeAreaView> 
   )
@@ -64,6 +97,32 @@ const styles = StyleSheet.create({
             height: 2,
         }
       },
+      textBg:{
+        fontSize: 15, 
+        fontWeight: 400, 
+        textAlign: "center", 
+        marginHorizontal: 20,
+        marginTop: 10
+      },
+      btnContainer:{
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 35,
+      },
+      btn: {
+        backgroundColor: "#529C4E",
+        alignItems: "center",
+        justifyContent: "center",
+        width: 150,
+        height: 40,
+        borderRadius: 15,
+        shadowColor: "#000",
+        shadowOpacity: 0.5,
+        shadowOffset:{
+            width: 0,
+            height: 2,
+        }
+      }
 })
 
 export default ModuleBgScreen
