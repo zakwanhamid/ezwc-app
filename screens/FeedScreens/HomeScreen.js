c38765391069d9a08d868b8054df4253498ea5d0
Zakwan Hamid <zakwanhamid@gmail.com>
2024-04-26 02:41:29 +0800

    added liked function and change the button color when post is liked

--- screens/FeedScreens/HomeScreen.js
+++ screens/FeedScreens/HomeScreen.js
@@ -1,10 +1,10 @@
-import { View, Text, TouchableOpacity, Button, StyleSheet, Image, TextInput, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, Button, StyleSheet, Image, FlatList } from 'react-native'
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { useNavigation, NavigationProp } from '@react-navigation/native'
 import { FIREBASE_AUTH, FIREBASE_DB } from '../../firebase';
 import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { collection, doc, getDoc, getDocs, onSnapshot, query, where } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, onSnapshot, query, updateDoc, where } from 'firebase/firestore';
 
 //this is the feed screen that will show all posts
 const HomeScreen = () => {
@@ -34,95 +34,19 @@ const HomeScreen = () => {
   useEffect(() => {
     const currentUserUid = FIREBASE_AUTH.currentUser.uid;
     const userRef = doc(collection(FIREBASE_DB, 'users'), currentUserUid);
-
+  
     const unsubscribe = onSnapshot(userRef, documentSnapshot => {
-        if (documentSnapshot.exists()) {
-            const userData = documentSnapshot.data(); // Get user data directly
-            setCurrentUser(userData);
-            setLoading(false);
-        } else {
-            // Handle case where user document doesn't exist
-            console.log("User document does not exist");
-        }
+      if (documentSnapshot.exists()) {
+        const userData = { id: documentSnapshot.id, ...documentSnapshot.data() }; // Include user ID in userData
+        setCurrentUser(userData);
+        setLoading(false);
+      } else {
+        // Handle case where user document doesn't exist
+        console.log("User document does not exist");
+      }
     });
     return () => unsubscribe();
   }, []);
-
-  
-
-  // const fetchFollowingPosts = async () => {
-  //   try {
-  //     // Assuming you have the followingUsers array with user IDs in state
-  //     const followingUsers = currentUser.following; // Sample user IDs
-  //     console.log(followingUsers)
-  //     const followingPostsPromises = followingUsers.map(async (followingUserId) => {
-  //       const postsQuery = query(collection(FIREBASE_DB, 'posts'), where('userId', '==', followingUserId));
-  //       const querySnapshot = await getDocs(postsQuery);
-        
-  //       const followingUserPosts = [];
-  //       querySnapshot.forEach((doc) => {
-  //         followingUserPosts.push({ id: doc.id, ...doc.data() });
-  //       });
-  //       console.log('following user posts',followingUserPosts)
-  //       return followingUserPosts;
-        
-  //     });
-
-  //     const followingPosts = await Promise.all(followingPostsPromises);
-  //     const mergedPosts = followingPosts.flat(); // Merge arrays of posts into one array
-  //     console.log('merged posts:', mergedPosts)
-  //     setFollowingUserPosts(mergedPosts);
-  //     setLoading(false);
-  //   } catch (error) {
-  //     console.error('Error fetching following user posts:', error);
-  //     setLoading(false);
-  //   }
-  // };
-
-  // const fetchFollowingPosts = async (followingUsers) => {
-  //   try {
-  //     const followingPostsPromises = followingUsers.map(async (followingUserId) => {
-  //       // Fetch the following user's data
-  //       const followingUserDocRef = doc(collection(FIREBASE_DB, 'users'), followingUserId);
-  //       const followingUserDocSnapshot = await getDoc(followingUserDocRef);
-  //       const followingUserData = followingUserDocSnapshot.exists()
-  //         ? { id: followingUserDocSnapshot.id, ...followingUserDocSnapshot.data() }
-  //         : null;
-  
-  //       // Fetch the posts of the following user
-  //       const postsQuery = query(collection(FIREBASE_DB, 'posts'), where('userId', '==', followingUserId));
-  //       const querySnapshot = await getDocs(postsQuery);
-  
-  //       const followingUserPosts = [];
-  //       querySnapshot.forEach((doc) => {
-  //         followingUserPosts.push({ id: doc.id, ...doc.data() });
-  //       });
-  
-  //       return { followingUserData, followingUserPosts };
-  //     });
-  
-  //     const followingPostsData = await Promise.all(followingPostsPromises);
-  //     console.log('Following Posts Data:', followingPostsData);
-  
-  //     // You can further process and set the state with the fetched data
-  //     // For example, if you want to separate followingUserData and followingUserPosts
-  //     const mergedPosts = followingPostsData.flatMap(({ followingUserPosts }) => followingUserPosts);
-  //     setFollowingUserPosts(mergedPosts);
-  //     console.log('merged posts:', mergedPosts)
-  
-  //     setLoading(false);
-  //   } catch (error) {
-  //     console.error('Error fetching following user posts:', error);
-  //     setLoading(false);
-  //   }
-  // };
-  
-  // // Usage in useEffect
-  // useEffect(() => {
-  //   if (currentUser.following) {
-  //     fetchFollowingPosts(currentUser.following);
-  //   }
-  // }, [currentUser.following]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -180,69 +104,131 @@ const HomeScreen = () => {
   
     fetchData();
   }, [currentUser.following]);
+
+  const handlePostLike = async (postId) => {
+    try {
+      const currentUserUid = FIREBASE_AUTH.currentUser.uid;
+      const postRef = doc(FIREBASE_DB, 'posts', postId);
+  
+      const postSnapshot = await getDoc(postRef);
+      if (postSnapshot.exists()) {
+        const postData = postSnapshot.data();
+        const postLikes = postData.likes || [];
+  
+        let updatedLikes;
+        if (postLikes.includes(currentUserUid)) {
+          // Remove current user id from post likes array
+          updatedLikes = postLikes.filter(id => id !== currentUserUid);
+        } else {
+          // Add current user id to post likes array
+          updatedLikes = [...postLikes, currentUserUid];
+        }
+  
+        // Update likes array in Firestore
+        await updateDoc(postRef, { likes: updatedLikes });
+        console.log('Post likes updated:', updatedLikes);
+  
+        // Fetch the updated post data again
+        const updatedPostSnapshot = await getDoc(postRef);
+        if (updatedPostSnapshot.exists()) {
+          const updatedPostData = updatedPostSnapshot.data();
+          const updatedPost = { id: updatedPostSnapshot.id, ...updatedPostData };
+  
+          // Update the mergedData state with the updated post data
+          const updatedMergedData = mergedData.map(userData => {
+            if (userData.posts) {
+              const updatedPosts = userData.posts.map(post => {
+                if (post.id === postId) {
+                  return updatedPost;
+                }
+                return post;
+              });
+              return { ...userData, posts: updatedPosts };
+            }
+            return userData;
+          });
+          setMergedData(updatedMergedData);
+        }
+      } else {
+        console.log('Post not found');
+      }
+    } catch (error) {
+      console.error('Error handling post like:', error);
+    }
+  };
+  
+  
   
 
   const renderPostContent = () => {
     return (
-      <View style={{paddingBottom:0}}>
-        <ScrollView>
-        <View>
-          {mergedData.map((userData) => (
-            <View key={userData.id}>
-              {userData.posts.sort((a, b) => b.timestamp - a.timestamp).map((post) => (
-                <View key={post.id} style={styles.postItem}>
-                <View style={{width:'15%', marginRight: '5%'}}>
-                    <Image source={require("../../assets/profilePic.jpeg")} style={styles.postAvatar}></Image>
-                </View>
-                <View style={{width:'80%', marginTop: 8}}>
-                    <View >
-                        <Text style={{fontSize: 15, fontWeight: 600}}>{userData.name}</Text>
-                        <Text style={{fontSize: 13, fontWeight: 200}}>{userData.email}</Text>
-                        <Text>{post.timestamp.toDate().toLocaleString('en-US', options)}</Text>
+      <View style={{paddingBottom:130}}>
+        <FlatList
+          data={mergedData}
+          keyExtractor={(userData) => userData.id}
+          renderItem={({ item: userData }) => (
+            <FlatList
+              data={userData.posts.sort((a, b) => b.timestamp - a.timestamp)}
+              keyExtractor={(post) => post.id}
+              renderItem={({ item: post }) => (
+                <View style={styles.postItem}>
+                  <View style={{ width: '15%', marginRight: '5%' }}>
+                    <Image source={require("../../assets/profilePic.jpeg")} style={styles.postAvatar} />
+                  </View>
+                  <View style={{ width: '80%', marginTop: 8 }}>
+                    <View>
+                      <Text style={{ fontSize: 15, fontWeight: 600 }}>{userData.name}</Text>
+                      <Text style={{ fontSize: 13, fontWeight: 200 }}>{userData.email}</Text>
+                      <Text>{post.timestamp.toDate().toLocaleString('en-US', options)}</Text>
                     </View>
-                    <View style={{marginTop:5}}>
-                        <Text >{post.text}</Text>
+                    <View style={{ marginTop: 5 }}>
+                      <Text>{post.text}</Text>
                     </View>
+
+                    <View style={styles.interactionCount}>
+                      <TouchableOpacity>
+                        { post.likes && post.likes.includes(currentUser.id) ? 
+                        (<AntDesign name="like1" size={20} color="#529C4E" />) :
+                        (<AntDesign name="like2" size={20} color="black" />)
+                        }
+                      </TouchableOpacity>
+
+                      <TouchableOpacity>
+                        <Text> {post.likes ? post.likes.length : 0} </Text>
+                      </TouchableOpacity>
+
+                      <TouchableOpacity>
+                        <FontAwesome name="comments-o" size={24} color="black" />
+                      </TouchableOpacity>
+                      
+                      <TouchableOpacity>
+                        <Text> {post.comment ? post.comment.length : 0} </Text>
+                      </TouchableOpacity>
+                    </View>
+
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={styles.buttonText}>Like</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={styles.buttonText}>Comment</Text>
-                        </TouchableOpacity>
+                      <TouchableOpacity 
+                        style={[
+                          styles.button, 
+                          post.likes && post.likes.includes(currentUser.id) ? styles.likedButton : null]} 
+                        onPress={() => handlePostLike(post.id)}
+                      >
+                        {post.likes && post.likes.includes(currentUser.id) ?(
+                          <Text style={[styles.buttonText, styles.buttonTextLiked ]}>Liked</Text>
+                          ):(
+                          <Text style={styles.buttonText}>Like </Text>
+                          )}
+                      </TouchableOpacity>
+                      <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Comment</Text>
+                      </TouchableOpacity>
                     </View>
-                </View>
-                </View>
-              ))}
-            </View>
-          ))}
-        </View> 
-          {/* {userData.sort((a, b) => b.timestamp - a.timestamp).map((post, index) => (
-              <View key={index} style={styles.postItem}>
-                  <View style={{width:'15%', marginRight: '5%'}}>
-                      <Image source={require("../../assets/profilePic.jpeg")} style={styles.postAvatar}></Image>
                   </View>
-                  <View style={{width:'80%', marginTop: 8}}>
-                      <View >
-                          <Text style={{fontSize: 15, fontWeight: 600}}>{currentUser.name}</Text>
-                          <Text style={{fontSize: 13, fontWeight: 200}}>{currentUser.email}</Text>
-                          <Text>{post.timestamp.toDate().toLocaleString('en-US', options)}</Text>
-                      </View>
-                      <View style={{marginTop:5}}>
-                          <Text >{post.text}</Text>
-                      </View>
-                      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                          <TouchableOpacity style={styles.button}>
-                              <Text style={styles.buttonText}>Like</Text>
-                          </TouchableOpacity>
-                          <TouchableOpacity style={styles.button}>
-                              <Text style={styles.buttonText}>Comment</Text>
-                          </TouchableOpacity>
-                      </View>
-                  </View>
-              </View>
-          ))} */}
-        </ScrollView>
+                </View>
+              )}
+            />
+          )}
+        />
       </View>
     );
   };
@@ -262,9 +248,7 @@ const HomeScreen = () => {
       </View>
 
       <View style={{paddingBottom:0}}>
-      <ScrollView>
-      {renderPostContent()}
-      </ScrollView>
+        {renderPostContent()}
       </View>
       
     </SafeAreaView>
@@ -311,17 +295,25 @@ const styles = StyleSheet.create({
       borderColor: "white",
       borderWidth: 2,
   },
-
   button: {
       width: "50%", 
       alignItems: "center",
       padding: 10,
       borderBottomColor: "#529C4E"
   },
-
   buttonText: {
       fontSize: 16,
       marginLeft: 8,
+  },
+  buttonTextLiked: {
+    color: "#529C4E",
+  },
+  interactionCount:{
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent:'flex-end',
+    marginRight: 20,
+    marginVertical: 10,
   },
 })
 
